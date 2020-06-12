5a0ea832be46de0dc1f815451bff0d10a4cf742e
Inchara Raj <Inchararaj@karthik@AIR>
2020-06-11 18:36:41 -0700

    Updates

---
+++ precache-manifest.bc0276edb6ee717d3527b16ef0ef0597.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e672604d7bc4241136dc2c64290f47",
+    "url": "/myreact_todoapp/index.html"
+  },
+  {
+    "revision": "0b78172b08f226fb17de",
+    "url": "/myreact_todoapp/static/css/main.c04ef0d2.chunk.css"
+  },
+  {
+    "revision": "40657ce019e2bdcc5801",
+    "url": "/myreact_todoapp/static/js/2.06b61ffd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreact_todoapp/static/js/2.06b61ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b78172b08f226fb17de",
+    "url": "/myreact_todoapp/static/js/main.237fea59.chunk.js"
+  },
+  {
+    "revision": "f5a7d551ad48d82824d3",
+    "url": "/myreact_todoapp/static/js/runtime-main.452bef24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreact_todoapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
